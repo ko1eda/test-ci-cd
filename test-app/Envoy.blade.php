e57537bf35d76cf8024647ff50f1fb90e3b2efd4
christopher-koleda <koleda.christopher@gmail.com>
2018-12-01 16:10:51 -0500

    Refactor envoy file and lock build directory in update_permissions task

--- test-app/Envoy.blade.php
+++ test-app/Envoy.blade.php
@@ -4,9 +4,10 @@
     $repository = 'git@gitlab.com:koleda/test-ci-cd.git';
     $releases_dir = '/srv/releases';
     $app_dir = '/srv/app';
+    $app_name= 'test-app';
     $release = date('YmdHis');
     $new_release_dir = $releases_dir .'/'. $release;
-    $app_mount = $releases_dir . '/' . $release . '/test-app'
+    $release_mount = $releases_dir . '/' . $release . '/' . $app_name;
 @endsetup
 
 @story('deploy')
@@ -28,21 +29,21 @@
 @task('build_images')
     echo 'Building container images'
     cd {{ $new_release_dir }}/build
-    export APP_MOUNT={{ $app_mount }}
+    export APP_MOUNT={{ $release_mount }}
     docker-compose -f docker-compose.base.yml -f docker-compose.prod.yml build
 @endtask
 
 @task('install_dependencies')
     echo "Starting deployment ({{ $release }})" 
     cd {{ $new_release_dir }}
-    export APP_MOUNT={{ $app_mount }}
+    export APP_MOUNT={{ $release_mount }}
     echo "Running composer install" 
     docker-compose -f build/docker-compose.base.yml -f build/docker-compose.prod.yml run --rm --user 1002 php-fpm bash -c "composer install --prefer-dist --no-scripts -q -o"
 @endtask
 
 @task('move_env')
     echo "Moving .env file from build to app"
-    cp {{ $new_release_dir }}/build/php/.env {{ $new_release_dir }}/test-app/.env 
+    cp {{ $new_release_dir }}/build/php/.env {{ $new_release_dir }}/{{ $app_name}}/.env 
 @endtask
 
 @task('update_symlinks')
@@ -60,23 +61,24 @@
     chgrp -h www-data {{ $app_dir }}
 @endtask
 
+{{-- Build containers and mount the newly linked app directory to them  --}}
 @task('build_containers')
     echo 'Building new containers'
     cd {{ $new_release_dir }}/build
-    export APP_MOUNT="/srv/app/test-app/"
+    export APP_MOUNT={{ $app_dir}}/{{ $app_name}}/
     docker-compose -f docker-compose.base.yml -f docker-compose.prod.yml down && \
     docker-compose -f docker-compose.base.yml -f docker-compose.prod.yml up -d 
 @endtask
 
-{{-- Doesn't work because files are owned by root and user doesn't have write permissions --}}
 @task('update_permissions')
-    echo "Updating file permissions" 
-    cd {{ $app_dir }}/test-app
+    echo "Updating app directory permissions" 
+    cd {{ $app_dir }}/{{ $app_name }}
     sudo chown -R deployer:www-data storage/ 
     sudo chmod -R 2770 storage/
-    {{-- sudo find storage/ -type f -exec chmod -R 660 {} + --}}
-@endtask
 
-{{-- @task('lock_build_directory')
-  Change permissions so that the build folder cannot be easily accessed
-@endtask --}}
+    echo "Updating build directory permissions" 
+    {{-- Then restrict permission on the build files --}}
+    cd {{ $app_dir }}
+    sudo chown -R root:ec2-user build
+    sudo chmod -R 770 build
+@endtask
